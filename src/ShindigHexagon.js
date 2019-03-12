52a0ead9363d5da3699d4f8d71384ce6143a34cc
Jon Scheiding <jonscheiding@gmail.com>
2019-03-12 16:00:18 -0400

    Added the lines

--- src/ShindigHexagon.js
+++ src/ShindigHexagon.js
@@ -35,15 +35,18 @@ export class ShindigHexagon extends Component {
     
     const id = `${row}-${col}`;
     
-    const rotation = ((row * 2) + (col * 2) + (row % 2)) * 60 * Math.PI / 180;
+    const rotation = (row + (col % 3)) * 60;
+    const rotationRadians = rotation * Math.PI / 180;
 
     const gradientOffset = {
-      x: 0.5 * Math.cos(rotation),
-      y: 0.5 * Math.sin(rotation)
+      x: 0.5 * Math.cos(rotationRadians),
+      y: 0.5 * Math.sin(rotationRadians)
     };
 
     return (
-      <g>
+      <g 
+      transform={`rotate(${rotation} ${center.x} ${center.y})`}
+      >
         <linearGradient id={id} 
           x1={0.5 - gradientOffset.x} y1={0.5 - gradientOffset.y}
           x2={0.5 + gradientOffset.x} y2={0.5 + gradientOffset.y}
@@ -54,6 +57,10 @@ export class ShindigHexagon extends Component {
         <polygon fill={`url(#${id})`}
           stroke={COLOR_FOREGROUND_OUTLINE} strokeWidth={STROKE_WIDTH} 
           points={pointsProperty} />
+        <line x1={points[1].x} y1={points[1].y} x2={points[4].x} y2={points[4].y}
+          stroke={COLOR_FOREGROUND_OUTLINE} strokeWidth={STROKE_WIDTH * 0.25} />
+        <line x1={points[1].x} y1={points[1].y} x2={points[3].x} y2={points[3].y}
+          stroke={COLOR_FOREGROUND_OUTLINE} strokeWidth={STROKE_WIDTH * 0.5} />
       </g>
     );
   }
